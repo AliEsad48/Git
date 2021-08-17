8ae03463ba7e1b273ec57de85d65d304503d5d65
Salih Bayram <83352523+AliEsad48@users.noreply.github.com>
2021-08-17 23:02:07 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,6 +2,8 @@ public class TestClass {
 
     public static void main(String[] args) {
         System.out.print("Git den merhaba!");
+        
+         System.out.print("GitHub dan merhaba!");
     }
 
 
